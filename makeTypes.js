438b26001f8adc4a0f49d12a492ca0f4992f79b3
Andrew Jones <thebinarysearchtree@gmail.com>
2025-02-13 19:45:24 +1000

    sample json columns

--- makeTypes.js
+++ makeTypes.js
@@ -1,16 +1,20 @@
 import fileSystem from './files.js';
 import makeFiles from './makeFiles.js';
 
-const makeTypes = async (db, paths) => {
+const makeTypes = async (db, paths, sampleData) => {
   try {
     if (!db.supports.files) {
       await makeFiles(paths);
     }
-    await db.makeTypes(fileSystem, paths);
+    await db.makeTypes(fileSystem, paths, sampleData);
     console.log('Types updated');
+    process.exit();
   }
   catch (e) {
-    console.log(e.message);
+    if (db.supports.closing) {
+      await db.close();
+    }
+    throw e;
   }
   finally {
     if (db.supports.closing) {
